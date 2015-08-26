c060f46d4f5d77eebd1e20738e44f20c98201cf9
James Nail <jrnail23@gmail.com>
2015-08-26 17:41:33 -0400

    Support more functionality from System.Diagnostics.Process in IProcess -- Enable raising events (Exited, ErrorDataReceived, OutputDataReceived) -- Redirect standard error -- HasExited property -- Environment variables for process

--- src/Cake.Core/IO/IProcess.cs
+++ src/Cake.Core/IO/IProcess.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace Cake.Core.IO
 {
@@ -8,6 +9,12 @@ namespace Cake.Core.IO
     /// </summary>
     public interface IProcess : IDisposable
     {
+        /// <summary>
+        /// Starts a process resource and associates it with a Process component.
+        /// </summary>
+        /// <returns>true if a process resource is started; false if no new process resource is started (for example, if an existing process is reused).</returns>
+        bool Start();
+
         /// <summary>
         /// Waits for the process to exit.
         /// </summary>
@@ -33,8 +40,45 @@ namespace Cake.Core.IO
         IEnumerable<string> GetStandardOutput();
 
         /// <summary>
+        /// Get the standard error output of process
+        /// </summary>
+        /// <returns>Returns process error output <see cref="ProcessSettings.RedirectStandardError">RedirectStandardError</see> is true</returns>
+        IEnumerable<string> GetStandardError();
+
+        /// <summary>
+        /// Gets the unique identifier for the associated process.
+        /// </summary>
+        /// <value>
+        /// The process identifier.
+        /// </value>
+        int ProcessId { get; }
+
+        /// <summary>
         /// Immediately stops the associated process.
         /// </summary>
         void Kill();
+
+        /// <summary>
+        /// Gets a value indicating whether this instance has exited.
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if this instance has exited; otherwise, <c>false</c>.
+        /// </value>
+        bool HasExited { get; }
+
+        /// <summary>
+        /// Occurs when process exits, when <see cref="ProcessSettings.EnableRaisingEvents">EnableRaisingEvents</see> is true.
+        /// </summary>
+        event EventHandler Exited;
+
+        /// <summary>
+        /// Occurs when an application writes to its redirected StandardError stream..
+        /// </summary>
+        event DataReceivedEventHandler ErrorDataReceived;
+
+        /// <summary>
+        /// Occurs when an application writes to its redirected StandardOutput stream..
+        /// </summary>
+        event DataReceivedEventHandler OutputDataReceived;
     }
 }
--- src/Cake.Core/IO/ProcessWrapper.cs
+++ src/Cake.Core/IO/ProcessWrapper.cs
@@ -18,6 +18,11 @@ namespace Cake.Core.IO
             _filterOutput = filterOutput ?? (source => "[REDACTED]");
         }
 
+        public bool Start()
+        {
+            return _process.Start();
+        }
+
         public void WaitForExit()
         {
             _process.WaitForExit();
@@ -52,12 +57,127 @@ namespace Cake.Core.IO
             }
         }
 
-        public void Kill()
+        public IEnumerable<string> GetStandardError()
         {
-            _process.Kill();
-            _process.WaitForExit();
+            string line;
+            while ((line = _process.StandardError.ReadLine()) != null)
+            {
+                _log.Warning("{0}", _filterOutput(line));
+                yield return line;
+            }
         }
 
+        public int ProcessId
+        {
+            get
+            {
+                return _process.Id;
+            }
+        }
+
+        public void Kill()
+        {
+            if (!HasExited)
+            {
+                _process.Kill();
+                _process.WaitForExit();
+            }
+        }
+
+        public bool HasExited
+        {
+            get
+            {
+                _process.Refresh();
+                return _process.HasExited;
+            }
+        }
+
+        event EventHandler IProcess.Exited
+        {
+            add
+            {
+                if (_process.EnableRaisingEvents)
+                {
+                    _process.Exited += value;
+                }
+            }
+            remove
+            {
+                if (_process.EnableRaisingEvents)
+                {
+                    _process.Exited -= value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Occurs when an application writes to its redirected StandardError stream.
+        /// </summary>
+        /// <remarks>
+        /// The ErrorDataReceived event indicates that the associated process has written to its redirected StandardError stream.
+        /// The event only occurs during asynchronous read operations on StandardError. To start asynchronous read operations, 
+        /// you must redirect the StandardError stream of a Process, add your event handler to the ErrorDataReceived event, 
+        /// and call BeginErrorReadLine. Thereafter, the ErrorDataReceived event signals each time the process writes a 
+        /// line to the redirected StandardError stream, until the process exits or calls CancelErrorRead.
+        /// <note>The application that is processing the asynchronous output should call the WaitForExit method to ensure that the output buffer has been flushed.</note>
+        /// </remarks>
+        event DataReceivedEventHandler IProcess.ErrorDataReceived
+        {
+            add
+            {
+                if (!_process.EnableRaisingEvents || !_process.StartInfo.RedirectStandardError)
+                {
+                    throw new InvalidOperationException(
+                        string.Format(
+                            "ErrorDataReceived event requires Process.EnableRaisingEvents and Process.StartInfo.RedirectStandardError to be true.  " +
+                            "In this instance, Process.EnableRaisingEvents is {0} and Process.StartInfo.RedirectStandardError is {1}",
+                            _process.EnableRaisingEvents, _process.StartInfo.RedirectStandardError));
+                }
+
+                _process.BeginErrorReadLine();
+                _process.ErrorDataReceived += value;
+            }
+            remove { _process.ErrorDataReceived -= value; }
+        }
+
+        /// <summary>
+        /// Occurs when an application writes to its redirected StandardOutput stream.
+        /// </summary>
+        /// <remarks>
+        /// The OutputDataReceived event indicates that the associated process has written to its redirected StandardOutput stream.
+        /// The event only occurs during asynchronous read operations on StandardOutput. To start asynchronous read operations, 
+        /// you must redirect the StandardOutput stream of a Process, add your event handler to the OutputDataReceived event, 
+        /// and call BeginOutputReadLine. Thereafter, the OutputDataReceived event signals each time the process writes a 
+        /// line to the redirected StandardOutput stream, until the process exits or calls CancelOutputRead.
+        /// <note>The application that is processing the asynchronous output should call the WaitForExit method to ensure that the output buffer has been flushed.</note>
+        /// </remarks>
+        event DataReceivedEventHandler IProcess.OutputDataReceived
+        {
+            add
+            {
+                if (!_process.EnableRaisingEvents || !_process.StartInfo.RedirectStandardOutput)
+                {
+                    throw new InvalidOperationException(
+                        string.Format(
+                            "OutputDataReceived event requires Process.EnableRaisingEvents and Process.StartInfo.RedirectStandardOutput to be true.  " +
+                            "In this instance, Process.EnableRaisingEvents is {0} and Process.StartInfo.RedirectStandardOutput is {1}",
+                            _process.EnableRaisingEvents, _process.StartInfo.RedirectStandardOutput));
+                }
+                _process.OutputDataReceived += value;
+                _process.BeginOutputReadLine();
+
+            }
+            remove
+            {
+                _process.OutputDataReceived -= value;
+            }
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        /// <remarks>This also closes the underlying process handle, but does not stop the process if it is still running.</remarks>
         public void Dispose()
         {
             _process.Dispose();
